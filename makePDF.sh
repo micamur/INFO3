310bd45d3cb01a82039ee593e9250d9a5a26cb70
Antoine Saget <antoinesaget19@gmail.com>
2018-10-14 14:08:39 +0200

    makePDF.sh : un script simple pour générer les pdf

---
+++ makePDF.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 2 ] 
+then
+    echo "Usage : $0 <input.md> <output.pdf>"
+    exit
+fi
+
+if [ ! -f "$1" ] 
+then
+    echo "File $1 not found"
+fi
+
+pandoc $1 -o $2 --variable margin-left=80px --variable margin-rigt=50px --variable margin-top=80px --variable margin-bottom=50px -s
