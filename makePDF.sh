e714b4cbcd1e3904cb3e74e7ddefdfc63d1c524c
Mica <micamurphy42@gmail.com>
2018-10-29 10:54:12 +0100

    Modification du script
    Possibilité de passer plusieurs fichiers en argument et git add lorsque la conversion en PDF a fonctionné

--- makePDF.sh
+++ makePDF.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -ne "1" ] && [ "$#" -ne "2" ]
+if (($# < 1))
 then
-    echo "Usage : $0 <input.md>"
-    echo "Usage : $0 <input.md> <output.pdf>"
+    echo "Usage : $0 input.md [input2.pdf] [...]"
     exit
 fi
 
-if [ ! -f "$1" ] 
-then
-    echo "File $1 not found"
-    exit
-fi
+OPTIONS="--variable margin-left=80px --variable margin-rigt=50px --variable margin-top=80px --variable margin-bottom=50px -s --template=.template.latex" # --highlight-style my.theme
 
-if [ $# -eq 1 ]
-then
-    fileNameMD="$1"
-    fileNamePDF="${fileNameMD%%.*}.pdf"
+for f in $*
+do
+  if [ ! -f "$f" ]
+  then
+      echo "File $f not found"
 
-    pandoc $fileNameMD -o $fileNamePDF --variable margin-left=80px --variable margin-rigt=50px --variable margin-top=80px --variable margin-bottom=50px -s --template=.template.latex
-    exit
-fi
+  else
+    echo "File $f found"
+    pandoc $f -o `basename $f md`pdf $OPTIONS
 
-if [ $# -eq 2 ]
-then
-    pandoc $fileNameMD -o $2 --variable margin-left=80px --variable margin-rigt=50px --variable margin-top=80px --variable margin-bottom=50px -s --template=.template.latex
-    exit
-fi
+    if (($? == 0))
+    then
+      git add $f `basename $f md`pdf
+      echo "Files $f and `basename $f md`pdf added"
+    fi
+  fi
+done
+
+exit
